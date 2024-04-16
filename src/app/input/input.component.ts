a03a0b3d9f160d8ecc8146e71090ab3d460d6ff9
DiegoAmorimDeSousa <diegoassousa@gmail.com>
2024-04-16 19:10:00 -0300

    feat: get input value

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css']
 })
 export class InputComponent {
+  urlInput: string = '';
 
+  short() {
+    console.log('Valor do input:', this.urlInput);
+  }
 }
