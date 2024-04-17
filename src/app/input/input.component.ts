fe733e7b2cf03e61a1916124c1ef2763528fcba3
DiegoAmorimDeSousa <diegoassousa@gmail.com>
2024-04-17 20:44:57 -0300

    feat: short url and copy

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -7,8 +7,26 @@ import { Component } from '@angular/core';
 })
 export class InputComponent {
   urlInput: string = '';
+  shortenedUrl: string = '';
 
   short() {
-    console.log('Valor do input:', this.urlInput);
+    fetch(`http://is.gd/create.php?url=${this.urlInput}&format=json`)
+      .then(response => response.json())
+      .then(data => {
+        this.shortenedUrl = data.shorturl;
+        console.log('SUA URL ENCURTADA AQUI:', this.shortenedUrl);
+      })
+      .catch(error => {
+        console.error('Erro ao fazer a solicitação:', error);
+      });
+  }
+
+  copyUrl() {
+    const el = document.createElement('textarea');
+    el.value = this.shortenedUrl;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
   }
 }
